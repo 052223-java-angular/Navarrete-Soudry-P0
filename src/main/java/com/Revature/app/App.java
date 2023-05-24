8ac9c9497480375bb9cbe1eadfa1473d4960185a
Thomas Navarrete <78869048+TNavarrete1@users.noreply.github.com>
2023-05-24 15:26:38 -0700

    Implement screen routing

--- src/main/java/com/Revature/app/App.java
+++ src/main/java/com/Revature/app/App.java
@@ -2,9 +2,14 @@ package com.Revature.app;
 
 import java.util.Scanner;
 
+import com.Revature.app.services.RouterService;
+
 public class App {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
+        RouterService router = new RouterService();
+        router.navigate("/home", scan);
+        scan.close();
     }
 
 }
--- src/main/java/com/Revature/app/services/RouterService.java
+++ src/main/java/com/Revature/app/services/RouterService.java
@@ -2,10 +2,19 @@ package com.Revature.app.services;
 
 import java.util.Scanner;
 
+import com.Revature.app.screens.HomeScreen;
+
 public class RouterService {
     public void navigate(String path, Scanner scan) {
         switch (path) {
             case "/home":
+                new HomeScreen().start(scan);
+                break;
+            case "/login":
+                break;
+            case "/register":
+                break;
+            default:
                 break;
         }
     }
