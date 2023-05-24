5d38c64f2ff24497d8d1ef19bc03452a2b1844f5
Alexander Soudry <soudryalex@gmail.com>
2023-05-24 15:55:38 -0400

    added code cleaning and a connection to registration.

--- src/main/java/com/Revature/app/App.java
+++ src/main/java/com/Revature/app/App.java
@@ -1,7 +1,22 @@
 package com.Revature.app;
 
+import com.Revature.app.Registration.*;
+
+import java.util.Scanner;
+
 public class App {
+
+  // Cleans up Code
+  private static void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+}
   public static void main(String args[]) {
+    RegistrationScreen r1 = new RegistrationScreen(null);
+    clearScreen();
     System.out.println("Welcome to eCommerce App!");
+    Scanner scan = new Scanner(System.in);
+    r1.testLogic(scan);
+    scan.close();
   }
 }
