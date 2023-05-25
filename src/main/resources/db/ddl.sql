dc907be92d67a436c2f0a550befccc86aeeeb646
Thomas Navarrete <78869048+TNavarrete1@users.noreply.github.com>
2023-05-25 11:30:39 -0700

    Update ddl and erd

--- src/main/resources/db/ddl.sql
+++ src/main/resources/db/ddl.sql
@@ -1,38 +1,64 @@
-DROP TABLE IF EXISTS orderHistory CASCADE;
-DROP TABLE IF EXISTS cart CASCADE;
-DROP TABLE IF EXISTS account CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS carts CASCADE;
+DROP TABLE If EXISTS categories CASCADE;
 DROP TABLE IF EXISTS products CASCADE;
+DROP TABLE If EXISTS reviews CASCADE;
+DROP TABLE If EXISTS orders CASCADE;
+DROP TABLE If EXISTS order_items CASCADE;
+
 
 CREATE TABLE users (
-    id VARCHAR(255) PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    password VARCHAR(100) UNIQUE
+    id VARCHAR PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+    password VARCHAR NOT NULL,
 );
 
-CREATE TABLE account (
-    id VARCHAR(255) PRIMARY KEY,
-    user_id VARCHAR(255) REFERENCES users (id)
+CREATE TABLE carts (
+    id VARCHAR PRIMARY KEY,
+    user_id VARCHAR NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE cart (
-    id VARCHAR(255) PRIMARY KEY,
-    account_id VARCHAR(255) REFERENCES account (id),
-    product_id VARCHAR(100) UNIQUE
+CREATE TABLE categories (
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL
 );
 
 CREATE TABLE products (
-    name VARCHAR(255) PRIMARY KEY,
-    category VARCHAR(100) NOT NULL,
-    price DECIMAL NOT NULL,
-    rating INT NOT NULL
+    id VARCHAR PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    price DECIMAL(8, 2) NOT NULL,
+    stock INTEGER NOT NULL,
+    category_id VARCHAR NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
-CREATE TABLE orderHistory (
-    id VARCHAR(255) PRIMARY KEY,
-    date DATE NOT NULL,
-    product_id VARCHAR(255) REFERENCES products (name),
-    account_id VARCHAR(255) REFERENCES account (id)
+CREATE TABLE reviews (
+    id VARCHAR PRIMARY KEY,
+    rating INTEGER NOT NULL,
+    description VARCHAR NOT NULL,
+    product_id VARCHAR NOT NULL,
+    user_id VARCHAR NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE TABLE orders (
+    id VARCHAR PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    total_cost DECIMAL(10, 2) NOT NULL,
+    user_id VARCHAR NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE order_items (
+    id VARCHAR PRIMARY KEY,
+    quantity VARCHAR NOT NULL,
+    price DECIMAL(8, 2) NOT NULL,
+    order_id VARCHAR NOT NULL,
+    product_id VARCHAR NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(id)
 );
 
 
