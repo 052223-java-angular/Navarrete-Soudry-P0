ba61071e69ed95d644f2cca1947178c5189fdbf9
Alexander Soudry <soudryalex@gmail.com>
2023-05-25 21:13:58 -0400

    Minor tweaks made while developing other component.

--- src/main/java/com/Revature/app/screens/HomeScreen.java
+++ src/main/java/com/Revature/app/screens/HomeScreen.java
@@ -16,10 +16,11 @@ public class HomeScreen implements IScreen {
 
         exit: {
             while (true) {
-                clearScreen();
+                // clearScreen();
                 System.out.println("Welcome to the app!");
                 System.out.println("\n[1] Login screen");
                 System.out.println("[2] register screen");
+                System.out.println("[3] fast-track-tomain");
                 System.out.println("[x] Exit");
 
                 System.out.print("\nEnter: ");
@@ -32,13 +33,16 @@ public class HomeScreen implements IScreen {
                     case "2":
                         router.navigate("/register", scan);
                         break;
+                    case "3":
+                        router.navigate("/mainApp", scan);
+                        break;
                     case "x":
                         System.out.println("\nGoodbye!");
                         break exit;
                     default:
-                        clearScreen();
-                        System.out.println("Invalid option!");
-                        System.out.print("\nPress enter to continue...");
+                        // clearScreen();
+                        // System.out.println("Invalid option!");
+                        // System.out.print("\nPress enter to continue...");
                         scan.nextLine();
                         break;
                 }
@@ -50,8 +54,8 @@ public class HomeScreen implements IScreen {
      * ------------------------ Helper methods ------------------------------
      */
 
-    private void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
+    // private void clearScreen() {
+    //     System.out.print("\033[H\033[2J");
+    //     System.out.flush();
+    // }
 }
